c50e4c03c38e325202994b0a9a1c9977484b6c95
Luke MacKenzie <luke.mackenzie@campjefferson.com>
2019-10-23 22:04:31 -0400

    first commit

---
+++ src/components/Ingredients/Search.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal';
+import useHttp from '../../hooks/http';
+import './Search.css';
+
+const Search = React.memo(props => {
+  const { onLoadIngredients } = props;
+  const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef();
+  const { isLoading, data, error, sendRequest, clear } = useHttp();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // enteredFilter below gets locked to the initial value within the timer
+      // use a ref to get the current value of the input
+      // this essentially creates and cleansup a timer on every keystroke
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ''
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        sendRequest(
+          'https://react-hooks-3d486.firebaseio.com/ingredients.json' + query,
+          'GET'
+        );
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [enteredFilter, inputRef, sendRequest]);
+
+  useEffect(() => {
+    if (!isLoading && !error && data) {
+      const loadedIngredients = [];
+      for (const key in data) {
+        loadedIngredients.push({
+          id: key,
+          title: data[key].title,
+          amount: data[key].amount
+        });
+      }
+      onLoadIngredients(loadedIngredients);
+    }
+  }, [data, isLoading, error, onLoadIngredients]);
+
+  return (
+    <section className="search">
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
+      <Card>
+        <div className="search-input">
+          <label>Filter by Title</label>
+          {isLoading && <span>Loading...</span>}
+          <input
+            ref={inputRef}
+            type="text"
+            value={enteredFilter}
+            onChange={event => setEnteredFilter(event.target.value)}
+          />
+        </div>
+      </Card>
+    </section>
+  );
+});
+
+export default Search;
